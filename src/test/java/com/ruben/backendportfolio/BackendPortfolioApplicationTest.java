7a0e8bc02191f848a7f069c5caf06c1f50f6c2f5
Rubén Ruiz Pérez <rubenrzprz@gmail.com>
2025-10-23 09:57:55 +0100

    test(api): add Spring context smoke test

---
+++ src/test/java/com/ruben/backendportfolio/BackendPortfolioApplicationTest.java
@@ -0,0 +1,11 @@
+package com.ruben.backendportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPortfolioApplicationTest {
+
+    @Test
+    void contextLoads() {}
+}
